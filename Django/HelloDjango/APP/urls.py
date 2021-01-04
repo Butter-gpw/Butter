2eb413ef74ce0dbd41e75e3a42d694d05d94fa1e
Butter-gpw <709749896@qq.com>
2021-01-04 10:22:25 +0800

    delete django

--- Django/HelloDjango/APP/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from APP import views
-
-urlpatterns = [
-    url(r'^addpersons/', views.add_person),
-    url(r'^getpersons', views.get_person)
-
-]
--- Django/HelloDjango/Two/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TowConfig(AppConfig):
-    name = 'Two'
